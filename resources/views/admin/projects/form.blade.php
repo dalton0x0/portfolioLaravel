33276e1062f83442e69a0c9195dadc9abdce4ffb
Chéridanh <divintsiela@gmail.com>
2024-03-23 13:31:00 +0100

    adding tinymce editor

--- resources/views/admin/projects/form.blade.php
+++ resources/views/admin/projects/form.blade.php
@@ -3,37 +3,44 @@
 @section('title', 'Administration')
 
 @section('content')
-    <div class="container p-4">
-        <div class="row justify-content-md-center">
-            <div class="col-md-12">
-                <div class="text-center">
-                    <h1>{{ $project->exists ? 'Modifier : ' . $project->name : 'Créer un project' }}</h1>
-                </div>
-                <form action="{{ route($project->exists ? 'admin.projects.update' : 'admin.projects.store', $project) }}" method="post">
-                    @csrf
-                    @method($project->exists ? 'put' : 'post')
-                    @include('partials.input', ['name' => 'title', 'label' => 'Titre du projet', 'placeholder' => 'Titre du projet'])
-                    @include('partials.input', ['type' => 'textarea','name' => 'content', 'label' => 'Contenu', 'placeholder' => 'Contenu'])
-                    <div>
-                        @if($project->exists)
-                            <button class="btn btn-outline-primary me-2 rounded">
-                                <i class="bi-check2-circle"></i>
-                            </button>
-                        @else
-                            <button class="btn btn-outline-success me-2 rounded">
-                                <i class="bi-plus-circle"></i>
-                            </button>
-                        @endif
+    <div class="my-5">
+        <div class="container p-4">
+            <div class="row justify-content-md-center">
+                <div class="col-md-12">
+                    <div class="text-center mb-5">
+                        <h1 class="display-5 fw-bolder">
+                            <span class="text-gradient d-inline">{{ $project->exists ? 'Modifier : ' . $project->name : 'Créer un project' }}</span>
+                        </h1>
                     </div>
-                </form>
+                    <form action="{{ route($project->exists ? 'admin.projects.update' : 'admin.projects.store', $project) }}" method="post">
+                        @csrf
+                        @method($project->exists ? 'put' : 'post')
+                        @include('partials.input', ['name' => 'title', 'label' => 'Titre du projet', 'placeholder' => 'Titre du projet'])
+                        @include('partials.input', ['type' => 'textarea','name' => 'content', 'label' => 'Contenu', 'placeholder' => 'Contenu'])
+                        <div>
+                            <button class="btn btn-primary me-2 rounded">
+                                <i class="bi-arrow-left-short"></i>
+                            </button>
+                            @if($project->exists)
+                                <button class="btn btn-outline-primary me-2 rounded">
+                                    <i class="bi-check2-circle"></i>
+                                </button>
+                            @else
+                                <button class="btn btn-outline-success me-2 rounded">
+                                    <i class="bi-plus-circle"></i>
+                                </button>
+                            @endif
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
     <script>
         tinymce.init({
             selector: 'textarea',
-            plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss markdown',
-            toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+            plugins: 'autoresize | anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss markdown',
+            toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat | language',
             tinycomments_mode: 'embedded',
             tinycomments_author: 'Author name',
             mergetags_list: [
@@ -41,16 +48,19 @@
                 { value: 'Email', title: 'Email' },
             ],
             ai_request: (request, respondWith) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant")),
+            content_langs: [
+                { title: 'French', code: 'fr' },
+                { title: 'English (US)', code: 'en_US' },
+                { title: 'English (UK)', code: 'en_UK' },
+                { title: 'Spanish', code: 'es' },
+                { title: 'Portuguese', code: 'pt' },
+            ],
+            spellchecker_language: 'fr',
+            spellchecker_dialog: true,
+            min_height: 500,
+            autoresize_bottom_margin: 0,
+            link_default_target: '_blank',
+            language: 'fr_FR',
         });
     </script>
-    {{--
-    <div id="content"></div>
-    <script>
-        $('#content').summernote({
-            placeholder: 'Hello Bootstrap 5',
-            tabsize: 2,
-            height: 100
-        });
-    </script>
-    --}}
 @endsection
--- resources/views/layouts/admin/base.blade.php
+++ resources/views/layouts/admin/base.blade.php
@@ -12,17 +12,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@100;200;300;400;500;600;700;800;900&amp;display=swap" rel="stylesheet" />
-    <script src="https://cdn.tiny.cloud/1/z4r3ap1derhp44d75sr713vfvy2jujygui1jlm3y9r8p2mtc/tinymce/7/tinymce.min.js" referrerpolicy="origin"></script>
-    {{--
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-
-    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.js"></script>
-    --}}
+    <script src="https://cdn.tiny.cloud/1/ho8r2ducjyomw56b7jz9bis86jljd725x689mjaxi3mi9xbx/tinymce/7/tinymce.min.js" referrerpolicy="origin"></script>
     <!-- Bootstrap icons -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css" rel="stylesheet" />
     <!-- style -->
