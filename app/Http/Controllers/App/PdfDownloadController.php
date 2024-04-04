c69665bae988f738a379b4fca44cec8e6b7e1345
Chéridanh <divintsiela@gmail.com>
2024-04-04 16:27:37 +0200

    adding extension .pdf to name file downloading

--- app/Http/Controllers/App/PdfDownloadController.php
+++ app/Http/Controllers/App/PdfDownloadController.php
@@ -9,22 +9,22 @@ class PdfDownloadController extends Controller
 {
     /* CV PDF */
     public function downloadCv () {
-        return Storage::download('public/pdf/cv/cv-cheridanh-tsiela.pdf', 'CV_Chéridanh_TSIELA');
+        return Storage::download('public/pdf/cv/cv-cheridanh-tsiela.pdf', 'CV_Chéridanh_TSIELA.pdf');
     }
 
     /* System PDF */
     public function donwloadSheetSytem () {
-        return Storage::download('public/pdf/bts/system/fiche-projet-systeme.pdf', 'Fiche_Projet_Système_Chéridanh');
+        return Storage::download('public/pdf/bts/system/fiche-projet-systeme.pdf', 'Fiche_Projet_Système_Chéridanh.pdf');
     }
     public function downloadProjectSystemSamba () {
-        return Storage::download('public/pdf/bts/system/samba.pdf', 'SAMBA_AD_Chéridanh');
+        return Storage::download('public/pdf/bts/system/samba.pdf', 'SAMBA_AD_Chéridanh.pdf');
     }
 
     /* Network PDF */
     public function donwloadSheetNetwork () {
-        return Storage::download('public/pdf/bts/network/fiche-projet-reseau.pdf', 'Fiche_Projet_Réseau_Chéridanh');
+        return Storage::download('public/pdf/bts/network/fiche-projet-reseau.pdf', 'Fiche_Projet_Réseau_Chéridanh.pdf');
     }
     public function downloadProjectNetwork () {
-        return Storage::download('public/pdf/system/network/project-network.pdf', 'Projet Réseau Chéridanh');
+        return Storage::download('public/pdf/system/network/project-network.pdf', 'Projet Réseau Chéridanh.pdf');
     }
 }
