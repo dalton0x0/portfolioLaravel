33037e2b77caea1ae905cf5bb9ac687ababdd8b3
Chéridanh <divintsiela@gmail.com>
2024-04-29 10:16:11 +0200

    just create user account on local P50 and refresh migrations database

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\AuthFormRequest;
-use App\Http\Requests\Admin\LoginRequest;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
