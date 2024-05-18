8e302ea4f0bedbea1b1321d55977b810872a9694
Chéridanh <divintsiela@gmail.com>
2024-05-18 21:37:41 +0200

    formating columns for formation apprenticeship and internship views

--- resources/views/app/projects/internship/index.blade.php
+++ resources/views/app/projects/internship/index.blade.php
@@ -6,7 +6,7 @@
     @foreach($periods as $period)
         <section class="album py-5 bg-body-tertiary">
             <div class="container px-4">
-                <div class="justify-content-center row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                <div class="justify-content-center row row-cols-1 row-cols-sm-2 row-cols-md-4 g-3">
                     @foreach($period->projects as $project)
                         <div class="col">
                             @include('partials.card-project')
