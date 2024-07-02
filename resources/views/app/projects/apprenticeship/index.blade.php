2efa80d53cf785cb176e788bfa63549810c79dd9
Chéridanh <divintsiela@gmail.com>
2024-07-02 13:00:53 +0200

    updating padding for internship and apprenticeship views

--- resources/views/app/projects/apprenticeship/index.blade.php
+++ resources/views/app/projects/apprenticeship/index.blade.php
@@ -3,20 +3,22 @@
 @section('title', 'Projets - Alternance')
 
 @section('content')
-    @foreach($projects as $categoryName => $categoryProjects)
-        <section class="album py-5 bg-body-tertiary">
-            <div class="container px-4">
-                <div class="text-center mb-5">
-                    <h1 class="display-5 fw-bolder mb-0"><span class="text-gradient d-inline">{{ ($categoryName) }}</span></h1>
+    <div class="pt-5">
+        @foreach($projects as $categoryName => $categoryProjects)
+            <section class="album pb-5 bg-body-tertiary">
+                <div class="container px-4">
+                    <div class="text-center mb-5">
+                        <h1 class="display-5 fw-bolder mb-0"><span class="text-gradient d-inline">{{ ($categoryName) }}</span></h1>
+                    </div>
+                    <div class="justify-content-center row row-cols-1 row-cols-sm-2 row-cols-md-4 g-3">
+                        @foreach($categoryProjects as $projects)
+                            <div class="col">
+                                @include('partials.card-project')
+                            </div>
+                        @endforeach
+                    </div>
                 </div>
-                <div class="justify-content-center row row-cols-1 row-cols-sm-2 row-cols-md-4 g-3">
-                    @foreach($categoryProjects as $projects)
-                        <div class="col">
-                            @include('partials.card-project')
-                        </div>
-                    @endforeach
-                </div>
-            </div>
-        </section>
-    @endforeach
+            </section>
+        @endforeach
+    </div>
 @endsection
